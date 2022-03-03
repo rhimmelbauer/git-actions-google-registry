d233a5c2ba339ead5f90b3657d9f7d627e81ce93
Roberto Himmelbauer <robertoh89@gmail.com>
2022-03-03 11:45:14 -0800

    version bump

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+package_metadata = {
+    'name': 'hello-world',
+    'version': '0.1.1',
+    'description': 'Testing out git actions with gcloud registry',
+    'long_description': 'Testing out git actions with gcloud registry',
+    'url': 'https://github.com/rhimmelbauer/django-user-notifications/',
+    'author': 'Roberto Himmelbauer',
+    'author_email': 'robertoh89@gmail.com',
+    'license': 'MIT license',
+    'classifiers': [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+    ],
+    'keywords': ['python', 'gcloud'],
+}
+
+setup(
+    packages=find_packages(include=['hello_world', 'hello_world.*']),
+    include_package_data=True,
+    **package_metadata
+)
